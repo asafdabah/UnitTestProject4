5a448aa2b6bc89d453b8441b965d6edd7a8fcf7f
asaf dabah <asafdabah@gmail.com>
2017-06-10 13:56:02 +0300

    selenium course
    table update

---
+++ Pizza_POF/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace Pizza_POF
+{
+
+    [TestClass]
+    public class UnitTest1
+    {
+                
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- UnitTestProject4/UnitTest1.cs
+++ UnitTestProject4/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
@@ -27,19 +28,62 @@ namespace UnitTestProject4
             //element = driver.FindElement(By.Name("NameExample"));
             //element.Click();
             //driver.Navigate().Back();
+            ////find an element using linkText
+            //element = driver.FindElement(By.LinkText("Click me using this link text!"));
+            //element.Click();
+            //driver.Navigate().Back();
+            ////find an element using Xpath
+            //element = driver.FindElement(By.XPath("//input[@type='radio'][2]"));
+            //element.Click();
+            //driver.Navigate().Back();
+            ////find an list using Xpath
+            //By xPath = By.XPath("//input[@type='radio']");
+            //IList<IWebElement> elements = driver.FindElements(xPath);
+            //table by id
+            locator = By.Id("htmlTableId");
+            element = driver.FindElement(locator);
+            IList<IWebElement> collectionOfRow = driver.FindElements(By.XPath("//*[@id='htmlTableId']/tbody/tr"));
+            //Using selenium : what is the salery of an sdet
 
-            element = driver.FindElement(By.LinkText("Click me using this link text!"));
-            element.Click();
-            driver.Navigate().Back();
+            int columnIndex = -1;
+            int columnCounter = 1;
+            const string Desired_column_header = "Salary";
+            const string Desired_value = "Automation Testing Architect";
+
+            for (int trIndex = 0; trIndex < collectionOfRow.Count; trIndex++)
+            {
+                var row = collectionOfRow[trIndex];
+                IList<IWebElement> AllCellsInRow = row.FindElements(By.XPath("./*"));
+
+                foreach (var cell in AllCellsInRow)
+                {
+                    if (cell.Text == Desired_column_header)
+                    {
+                        columnIndex = columnCounter;
+                    }
+
+                    if (cell.Text == Desired_value)
+                    {
+                        string salryLocator =
+                            string.Format(".//*[@id='htmlTableId']/tbody/tr[{0}]/td[{1}]", trIndex +1, columnIndex);
+                        IWebElement Salery = driver.FindElement(By.XPath(salryLocator));
+                        Console.WriteLine(Salery.Text);
+                    }
+                    columnCounter++;
+                }
+
+
+            }
+
         }
 
         [TestCleanup]
         public void CleanUp()
-       {
+        {
 
             driver.Close();
             driver.Quit();
-                       
+
         }
     }
 }
